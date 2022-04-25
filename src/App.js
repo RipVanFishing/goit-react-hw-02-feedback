4a2ecd579d9afc570c034427b83956af32e69672
Denis Serkhel <ripvanfish27@gmail.com>
2022-04-25 20:23:53 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import FeedbackOptions from './components/FeedbackOption/FeedbackOption';
+import Statistics from './components/Statistic/Statistic'
+import Section from './components/Section/Section'
+import Notification from './components/Notification/Notification'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  };
+
+ totalFeeds = () => {
+  const {good, neutral, bad} = this.state
+  return good + neutral + bad
+
+ } 
+
+ positivePercentage = () => {
+  const {good} = this.state
+  const total = this.totalFeeds()
+  return Math.trunc(good / total * 100) 
+
+ }
+
+  
+ 
+addFeed = (props) => {
+    const {name} = props.currentTarget        
+    this.setState(prevState => ({
+      [name]:prevState[name] + 1
+    }));
+
+  };
+
+
+  render() 
+    {
+      const {good, neutral, bad} = this.state
+      const total = this.totalFeeds()
+      const positivePercentage = this.positivePercentage()
+     
+    return (
+<>
+<Section title="Please leave feedback">
+  <FeedbackOptions options={Object.keys(this.state)} addFeed={this.addFeed} />  
+</Section>
+<Section title="Statistics">
+  {total ?
+  <Statistics good={good} neutral={neutral} bad={bad} total={total} positivePercentage={positivePercentage ? positivePercentage : 0} /> 
+  : <Notification message="There is no feedback"></Notification>}
+      
+</Section>
+
+
+</>
+
+
+    )
+  }
 }
+
 
 export default App;
